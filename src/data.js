772f4dab09b7ad8a26300079a410bcded8e0c212
Aytac <aytac@Aytac-MacBook-Air.local>
2021-09-30 22:59:42 +0400

    React-Editable-Table

---
+++ src/data.js
@@ -0,0 +1,97 @@
+const employeesData = [
+    {
+      id: '001D',
+      name: 'Jon',
+      surname: 'Smite',
+      dateOfBirth:'1994-07-12',
+      position: 'Support Manager',
+      phoneNumber: '+994 55 123 43 43',
+      isDeleted: false,
+      inputId: '001D'
+    },
+    {
+        id: '002J',
+        name: 'Cesi',
+        surname: 'Newhall',
+        dateOfBirth:'1998-03-24',
+        position: 'Software engineer',
+        phoneNumber: '+994 55 721 23 23',
+        isDeleted: false,
+        inputId: '002J'
+    },
+    {
+        id: '003T', 
+        name: 'White',
+        surname: 'Wallter',
+        dateOfBirth:'1982-04-02',
+        position: 'Information Security',
+        phoneNumber: '+994 55 234 23 23',
+        isDeleted: false,
+        inputId: '003T'
+    },
+    {
+        id: '004F',
+        name: 'Anna',
+        surname: 'Mapstone',
+        dateOfBirth:'1996-01-18',
+        position: 'Data Quality',
+        phoneNumber: '+994 55 239 78 78',
+        isDeleted: false,
+        inputId: '004F'
+    },
+    {
+        id: '005F',
+        name: 'Bethany',
+        surname: 'Dence',
+        dateOfBirth:'1992-02-11',
+        position: 'Architect',
+        phoneNumber: '+994 45 454 23 23',
+        isDeleted: false,
+        inputId: '005F'
+    },
+    {
+        id: '006F',
+        name: 'Mike',
+        surname: 'Mileham',
+        dateOfBirth:'1993-08-19',
+        position: 'Support Analyst',
+        phoneNumber: '+994 55 564 76 55',
+        isDeleted: false,
+        inputId: '006F'
+    },
+    {
+        id: '007F', 
+        name: 'Jessi',
+        surname: 'Roberts',
+        dateOfBirth:'1986-02-10',
+        position: 'UI/UX designer',
+        phoneNumber: '+994 55 678 98 08',
+        isDeleted: false,
+        inputId: '007F'
+    },
+    {
+        id: '008S',
+        name: 'Deni',
+        surname: 'Denver',
+        dateOfBirth:'1991-06-18',
+        position: 'Front-end developer',
+        phoneNumber: '+994 55 654 76 76',
+        isDeleted: false,
+        inputId: '008S'
+    },
+    {
+        id: '009F',
+        name: 'Connor',
+        surname: 'Wright',
+        dateOfBirth:'1998-03-01',
+        position: 'Back-end developer',
+        phoneNumber: '+994 56 678 94 35',
+        isDeleted: false,
+        inputId: '009F'
+    },
+   
+ 
+
+  ];
+
+  export default employeesData
